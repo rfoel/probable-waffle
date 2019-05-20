9d00db7495e81802d1ec7a949c5e390b2918c7ca
Rafael Franco <rafaelfr@outlook.com>
2019-05-20 20:34:11 -0300

    Add next.config to expose env variable

--- libs/request.js
+++ libs/request.js
@@ -1,5 +1,8 @@
+import getConfig from 'next/config';
 import { GraphQLClient } from 'graphql-request';
 
+const { publicRuntimeConfig } = getConfig();
+
 export default new GraphQLClient('https://api.github.com/graphql', {
-  headers: { Authorization: `bearer ${process.env.GITHUB_TOKEN}` },
+  headers: { Authorization: `bearer ${publicRuntimeConfig.GITHUB_TOKEN}` },
 });
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicRuntimeConfig: {
+    GITHUB_KEY: process.env.GITHUB_KEY,
+  },
+};
